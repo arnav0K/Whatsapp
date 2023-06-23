53867a4b9ad6fba60d21099e0d7b298fba4e6cb8
Arnav4245 <101010370+Arnav4245@users.noreply.github.com>
2023-06-23 09:45:08 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import { lazy, Suspense } from 'react';
+
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+//components
+import UserProvider from './context/UserProvider';
+import AccountProvider from './context/AccountProvider';
+
+import Loader from './components/loader/Loader';
+
+const Messenger = lazy(() => import('./components/Messenger'));
+
+function App() {
+
+  const clientId = '542447154456-3orq282go4h3j415o0ovoita38bm6phh.apps.googleusercontent.com';
+
+  return (
+    <GoogleOAuthProvider clientId={clientId}>
+      <UserProvider>
+        <AccountProvider>
+          <Suspense fallback={<Loader />}>
+            <Messenger/>
+          </Suspense>
+        </AccountProvider>
+      </UserProvider>
+    </GoogleOAuthProvider>
+  );
+}
+
+export default App;
